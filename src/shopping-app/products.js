6df2a290be013cdb01a19a415499a80dbc00802f
Omotayo21 <110260275+Omotayo21@users.noreply.github.com>
2023-10-16 19:19:34 +0100

    Update products.js

--- src/shopping-app/products.js
+++ src/shopping-app/products.js
@@ -1,8 +1,4 @@
 import product1 from './assets/1.png'
-/*import product2 from './assets/2.png'
-import product3 from './assets/3.png'
-import product4 from './assets/4.png'
-import product5 from './assets/5.png'*/
 import product6 from './assets/6.png'
 import product7 from './assets/7.png'
 import product8 from './assets/8.png'
@@ -30,28 +26,28 @@ export const PRODUCTS = [
        {
         id : 11,
         productName : "Iphone 14 pro max",
-        price : 1200000,
+        price : 1200,
         productImage: product11,
          productDescription: 'lorem ipsum dolor sit autocompleteClasses, consectur adipising elit , sed do aluismod tempor incididunt ut laboremet dolore magna aliqua . Ut enim amd minim veniam , quis nostruda extacitatiom ullamco laboris nisi ut aliquip ex ca cocarrying amountodo consequat. duis aite irure dolor in reprehendent',
        },
        {
         id : 12,
         productName : "Iphone X",
-        price : 350000,
+        price : 3500,
         productImage: product12,
          productDescription: 'lorem ipsum dolor sit autocompleteClasses, consectur adipising elit , sed do aluismod tempor incididunt ut laboremet dolore magna aliqua . Ut enim amd minim veniam , quis nostruda extacitatiom ullamco laboris nisi ut aliquip ex ca cocarrying amountodo consequat. duis aite irure dolor in reprehendent',
        },
        {
         id : 6,
         productName : "Jean jacket",
-        price : 15000,
+        price : 1500,
         productImage: product6,
          productDescription: 'lorem ipsum dolor sit autocompleteClasses, consectur adipising elit , sed do aluismod tempor incididunt ut laboremet dolore magna aliqua . Ut enim amd minim veniam , quis nostruda extacitatiom ullamco laboris nisi ut aliquip ex ca cocarrying amountodo consequat. duis aite irure dolor in reprehendent',
        },
        {
         id : 7,
         productName : "Acer laptop",
-        price : 350000,
+        price : 3500,
         productImage: product7,
          productDescription: 'lorem ipsum dolor sit autocompleteClasses, consectur adipising elit , sed do aluismod tempor incididunt ut laboremet dolore magna aliqua . Ut enim amd minim veniam , quis nostruda extacitatiom ullamco laboris nisi ut aliquip ex ca cocarrying amountodo consequat. duis aite irure dolor in reprehendent',
        },
@@ -70,31 +66,4 @@ export const PRODUCTS = [
      productImage: product1,
       productDescription: 'lorem ipsum dolor sit autocompleteClasses, consectur adipising elit , sed do aluismod tempor incididunt ut laboremet dolore magna aliqua . Ut enim amd minim veniam , quis nostruda extacitatiom ullamco laboris nisi ut aliquip ex ca cocarrying amountodo consequat. duis aite irure dolor in reprehendent',
     },]
-    /*{
-        id : 2,
-        productName : "Air jordan shoes",
-        price : 25000,
-        productImage: product2,
-       },
-       {
-        id : 3,
-        productName : "Football boots",
-        price : 20000,
-        productImage: product3,
-       },
-       {
-        id : 4,
-        productName :"Airpods pro",
-        price : 22000,
-        productImage: product4,
-       },
-       {
-        id : 5,
-        productName : "OLED 65' inch TV",
-        price : 102000,
-        productImage: product5,
-       },
-     
-
-
-]*/
+    
