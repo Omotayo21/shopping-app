215a7ebe1b4105dfaf53125b243c89d4717576aa
Omotayo21 <110260275+Omotayo21@users.noreply.github.com>
2023-10-16 19:46:22 +0100

    Update navbar.jsx

--- src/shopping-app/components/navbar.jsx
+++ src/shopping-app/components/navbar.jsx
@@ -90,9 +90,7 @@ const Navbar = ({PRODUCTS}) => {
       <li>
         <Link to="/about" href="#" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 focus:text-blue-500" aria-current="page">About</Link>
       </li>
-      <li>
-         <Link to="/contact" href="#" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 focus:text-blue-500" aria-current="page">Services</Link>
-      </li>
+      
       <li>
         <Link to="/contact" href="#" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 focus:text-blue-500" aria-current="page">Contact</Link>
       </li>
@@ -116,9 +114,7 @@ const Navbar = ({PRODUCTS}) => {
       <li>
         <Link to="/about" href="#" className="block px-3 py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 focus:text-blue-500">About</Link>
       </li>
-      <li>
-        <a href="#" className="block px-3 py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 sm:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 focus:text-blue-500">Services</a>
-      </li>
+      
       <li>
         <Link to="/contact" href="#" className="block px-3 py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 focus:text-blue-500" aria-current="page">Contact</Link>
       </li>
